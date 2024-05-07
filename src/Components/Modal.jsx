ae028b2cdf30340f8ed514e2e18fe2955728d359
Emilio Jimenez <emiliojimenez@Emilios-MBP.lan>
2024-05-06 23:52:34 -0700

    added modal form starting point

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,18 +1,32 @@
-import styles from './Modal.module.css';
+import styles from "./Modal.module.css";
 
-export default function Modal(props){
-    if (props.isOpen === false) return null;
+export default function Modal(props) {
+  if (props.isOpen === false) return null;
 
-    const handleClick = () => {
-        props.onClose();
-    }
+  const handleClick = () => {
+    props.onClose();
+  };
 
-    return (
-        <div className={styles.modal}>
-            <div className={styles.modalContent}>
-                <span onClick={handleClick} className={styles.close}>x</span>
-                <p>Does this work ?</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modalContent}>
+        <span onClick={handleClick} className={styles.close}>
+          x
+        </span>
+        <p>Does this work ?</p>
+      </div>
+
+      <div className={styles.modalContent}>
+        <span class="close">&times;</span>
+        <h2>Modal Title</h2>
+        <form id="myForm">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+          <label for="steps">Steps:</label>
+          <textarea id="steps" name="steps" required></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
