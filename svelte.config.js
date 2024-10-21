7cad39c4b705a0708e793901af0b2e4ffe91264d
Emilio Jimenez <ejimenez@DiamondGame.com>
2024-10-21 13:31:56 -0700

    trying vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,8 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
-const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'  // This will serve index.html for dynamic routes
-		}),
-		// Enable prerender for all static routes
-		prerender: {
-			entries: ['*']  // Prerender all static routes
-		}
-	}
+export default {
+  kit: {
+    adapter: adapter(),
+    // other configurations
+  }
 };
-
-export default config;
